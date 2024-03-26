d02cadfc0af775e13a3a45f003fc90c92812c871
sefaonder <ondersefa111@gmail.com>
2024-03-26 16:02:05 +0300

    create Form init & get all forms init

--- src/controllers/InternFormControllers/internForm.controller.ts
+++ src/controllers/InternFormControllers/internForm.controller.ts
@@ -4,9 +4,47 @@ import { BadRequestError } from "../../errors/BadRequestError";
 
 export const addNewForm = async (req, res, next) => {
   try {
-    const { start_date, end_date } = req.body;
+    const { start_date, end_date, edu_year } = req.body;
 
-    res.status(201).json({ message: "User succesfully created" });
+    const student_id = req.id;
+
+    const date1 = new Date(start_date);
+    const date2 = new Date(end_date);
+
+    const comission_user = await prisma.user.findFirst({
+      where: { user_type: "ADMIN" },
+    });
+
+    const newForm = await prisma.internForm.create({
+      data: {
+        start_date: date1,
+        end_date: date2,
+        edu_year: edu_year,
+        total_work_day: 40,
+        student: {
+          connect: {
+            id: student_id,
+          },
+        },
+
+        follow_up: {
+          connect: {
+            id: comission_user.id,
+          },
+        },
+      },
+    });
+
+    res.status(201).json({ message: newForm });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const getAllForms = async (req, res, next) => {
+  try {
+    const allForms = await prisma.internForm.findMany();
+    res.status(200).json({ message: allForms });
   } catch (e) {
     next(e);
   }
--- src/routes/internFormRoute.ts
+++ src/routes/internFormRoute.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { validateRequestSchema } from "../middlewares/validationHandler.middleware";
 import { protect } from "../middlewares/auth.middleware";
+import {
+  addNewForm,
+  getAllForms,
+} from "../controllers/InternFormControllers/internForm.controller";
 
 const internFormRouter = express.Router();
 
-internFormRouter.post("/addNewForm");
+internFormRouter.post("/addNewForm", protect, addNewForm);
+
+internFormRouter.get("/getForms", protect, getAllForms);
 
 export default internFormRouter;
