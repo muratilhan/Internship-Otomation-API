0aa0e66a20f6af35decab686325e63cd6d6cec62
sefaonder <ondersefa111@gmail.com>
2024-05-19 20:21:15 +0300

    try catch added on profile controllers

--- src/controllers/profileControllers/profile.controller.ts
+++ src/controllers/profileControllers/profile.controller.ts
@@ -4,48 +4,56 @@ import { AuthenticationError } from "../../errors/AuthenticationError";
 import { BadRequestError } from "../../errors/BadRequestError";
 
 export const getMyProfile = async (req, res, next) => {
-  const userId = req.id;
+  try {
+    const userId = req.id;
 
-  if (!userId) {
-    throw new AuthenticationError(errorCodes.NOT_AUTHENTICATE);
+    if (!userId) {
+      throw new AuthenticationError(errorCodes.NOT_AUTHENTICATE);
+    }
+
+    const userInfo = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        user_type: true,
+        name: true,
+        last_name: true,
+        tc_number: true,
+        school_number: true,
+      },
+    });
+
+    if (!userInfo) {
+      throw new BadRequestError(errorCodes.NOT_FOUND);
+    }
+
+    return res.status(200).json({ data: userInfo });
+  } catch (error) {
+    next(error);
   }
-
-  const userInfo = await prisma.user.findUnique({
-    where: { id: userId },
-    select: {
-      id: true,
-      user_type: true,
-      name: true,
-      last_name: true,
-      tc_number: true,
-      school_number: true,
-    },
-  });
-
-  if (!userInfo) {
-    throw new BadRequestError(errorCodes.NOT_FOUND);
-  }
-
-  return res.status(200).json({ data: userInfo });
 };
 
 export const updateMyProfile = async (req, res, next) => {
-  const userId = req.id;
-  const { name, lastName, tcNumber } = req.body;
+  try {
+    const userId = req.id;
+    const { name, lastName, tcNumber } = req.body;
 
-  if (!userId) {
-    throw new AuthenticationError(errorCodes.NOT_AUTHENTICATE);
+    if (!userId) {
+      throw new AuthenticationError(errorCodes.NOT_AUTHENTICATE);
+    }
+    const userInfo = await prisma.user.findUnique({ where: { id: userId } });
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userInfo.id },
+      data: {
+        name: name,
+        last_name: lastName,
+        tc_number: tcNumber,
+      },
+    });
+
+    res.json({ message: "Profile updated succesfully" });
+  } catch (error) {
+    next(error);
   }
-  const userInfo = await prisma.user.findUnique({ where: { id: userId } });
-
-  const updatedUser = await prisma.user.update({
-    where: { id: userInfo.id },
-    data: {
-      name: name,
-      last_name: lastName,
-      tc_number: tcNumber,
-    },
-  });
-
-  res.json({ message: "Profile updated succesfully" });
 };
